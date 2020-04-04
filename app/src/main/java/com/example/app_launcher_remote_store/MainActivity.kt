a8d3903bdd2ffb505da1bea821943decae0963a8
xylo99 <thandy1@umbc.edu>
2020-04-03 20:46:00 -0400

    Cleaned up a whole lot. App should be more functional now. Actual encryption is used now.

--- app/src/main/java/com/example/app_launcher_remote_store/MainActivity.kt
+++ app/src/main/java/com/example/app_launcher_remote_store/MainActivity.kt
@@ -1,31 +1,138 @@
 package com.example.app_launcher_remote_store
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.RemoteException
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import de.blinkt.openvpn.LaunchVPN
 import de.blinkt.openvpn.VpnProfile
-import de.blinkt.openvpn.core.ConfigParser
-import de.blinkt.openvpn.core.ConnectionStatus
-//import de.blinkt.openvpn.core.IOpenVPNServiceInternal
-import de.blinkt.openvpn.core.ProfileManager
-import de.blinkt.openvpn.core.VpnStatus
-import java.io.BufferedReader
-import java.io.InputStream
+import de.blinkt.openvpn.core.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.*
+import java.nio.charset.Charset
+import java.util.*
 
 class MainActivity : AppCompatActivity(), VpnStatus.ByteCountListener, VpnStatus.StateListener {
 
-    //var mService: IOpenVPNServiceInternal? = null
+    lateinit var mService: IOpenVPNServiceInternal
 
-    var inputStream: InputStream? = null
-    var bufferedReader: BufferedReader? = null
-    var cp: ConfigParser? = null
-    var vp: VpnProfile? = null
-    var pm: ProfileManager? = null
-    var thread: Thread? = null
+    lateinit var inputStream: InputStream
+    lateinit var bufferedReader: BufferedReader
+    lateinit var cp: ConfigParser
+    lateinit var vp: VpnProfile
+    lateinit var pm: ProfileManager
+    lateinit var thread: Thread
 
+
+    private fun startVpn(VPNFile: String) {
+        try {
+            try {
+                assert(VPNFile != null)
+                inputStream = ByteArrayInputStream(
+                    VPNFile.toByteArray(
+                        Charset.forName("UTF-8")
+                    )
+                )
+            } catch (e: Exception) {
+                    e.printStackTrace()
+            }
+            try { // M8
+                assert(inputStream != null)
+                bufferedReader =
+                    BufferedReader(InputStreamReader(inputStream , Charset.forName("UTF-8")))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            cp = ConfigParser()
+            try {
+                cp.parseConfig(bufferedReader)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            vp = cp.convertProfile()
+//            vp.mAllowedAppsVpnAreDisallowed = true
+            val En = EncryptData()
+            val AppValues = getSharedPreferences("app_values", 0)
+
+            val randPW = UUID.randomUUID().toString().substring(0,15)
+
+            val AppDetailsValues: String =
+                En.decrypt(AppValues.getString("app_details", "NA"), randPW)
+            try {
+                val json_response = JSONObject(AppDetailsValues)
+                val jsonArray = json_response.getJSONArray("blocked")
+                for (i in 0 until jsonArray.length()) {
+                    val json_object = jsonArray.getJSONObject(i)
+                    vp.mAllowedAppsVpn.add(json_object.getString("app"))
+                    Log.e("packages", json_object.getString("app"))
+
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+            try {
+                vp.mName = Build.MODEL
+            } catch (e: Exception) {
+                e.printStackTrace()
+
+            }
+
+            try {
+                pm = ProfileManager.getInstance(this@MainActivity)
+                pm.addProfile(vp)
+                pm.saveProfileList(this@MainActivity)
+                pm.saveProfile(this@MainActivity, vp)
+                vp = pm.getProfileByName(Build.MODEL)
+                val intent = Intent(applicationContext, LaunchVPN::class.java)
+                intent.putExtra(LaunchVPN.EXTRA_KEY, vp.getUUID().toString())
+                intent.action = Intent.ACTION_MAIN
+                startActivity(intent)
+                App.isStart = false
+            } catch (e: Exception) {
+                e.printStackTrace()
+
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun stop_vpn() {
+        App.connection_status = 0
+        OpenVPNService.abortConnectionVPN = true
+        ProfileManager.setConntectedVpnProfileDisconnected(this)
+        if (mService != null) {
+            try {
+                mService.stopVPN(false)
+            } catch (e: RemoteException) {
+                val params = Bundle()
+                params.putString("device_id", App.device_id)
+                params.putString("exception", "MA18$e")
+                //                mFirebaseAnalytics.logEvent("app_param_error", params);
+            }
+            try {
+                pm = ProfileManager.getInstance(this)
+                vp = pm.getProfileByName(Build.MODEL)
+                pm.removeProfile(this, vp)
+            } catch (e: Exception) {
+                val params = Bundle()
+                params.putString("device_id", App.device_id)
+                params.putString("exception", "MA17$e")
+                //mFirebaseAnalytics.logEvent("app_param_error", params);
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-    
+        try {
+            //startVpn(File)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun updateState(
